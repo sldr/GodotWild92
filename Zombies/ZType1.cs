52ec70e787e7639f10908f4559886c2e1611585f
Stephen L. De Rudder <Stephen@SLDR.com>
2026-04-13 04:17:01 -0500

    typo

--- Zombies/ZType1.cs
+++ Zombies/ZType1.cs
@@ -4,7 +4,7 @@ using System;
 public partial class ZType1 : RigidBody3D
 {
 
-    public void _on_body_entered(Node bodyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa)
+    public void _on_body_entered(Node body)
     {
         int i = 5;
     }
