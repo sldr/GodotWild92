a112758ba37f406e7703659fd3ebb88766212444
Stephen L. De Rudder <Stephen@SLDR.com>
2026-04-16 23:29:02 -0500

    Add MultiMeshInstance3D

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -11,6 +11,8 @@ public partial class Game : Node3D
     [Export] public Player player;
     [Export] public PackedScene ZombieType1;
     [Export] public PackedScene ZombieType2;
+    [Export] public MultiMeshInstance3D MultiMeshInstance;
+    private MultiMesh mm;
 
     public void ZombieHit(Zombie zombie)
     {
@@ -21,7 +23,6 @@ public partial class Game : Node3D
 
     public void ZombieSpawner(bool preGameStart = false)
     {
-        MultiMeshInstance3D aaa;
         if (preGameStart) {
             GD.Print("PreGameStart");
             for (int i=0; i< InitialSpawnCount; i++) {
@@ -41,7 +42,29 @@ public partial class Game : Node3D
                 z.GlobalPosition = spawnPosition;
             }
         }
+    }
 
+    public void ZombieSpawnerMultiMesh(MultiMesh mm, bool preGameStart = false)
+    {
+        if (preGameStart) {
+            GD.Print("PreGameStart");
+            for (int i = 0; i < mm.InstanceCount; i++) {
+                float angle = (float)GD.RandRange(0, Mathf.Tau); // Random angle (0 to 360 degrees in radians)
+                float t = (float)GD.Randf();
+                float dist = (float)Mathf.Lerp(10d, SpawnDespawnRadius, Mathf.Sqrt(t)); // Random distance between 10 n SpawnDespawnRadius
+                //float dist = (float)GD.RandRange(10d, SpawnDespawnRadius); // Random distance between 10 n SpawnDespawnRadius
+                // Convert polar → Cartesian (XZ plane)
+                Vector3 offset = new Vector3(
+                    Mathf.Cos(angle),
+                    0,
+                    Mathf.Sin(angle)
+                ) * dist + Vector3.Up;
+                Vector3 spawnPosition = GlobalPosition + offset;
+                var transform = Transform3D.Identity;
+                transform.Origin = spawnPosition;
+                mm.SetInstanceTransform(i, transform);
+            }
+        }
     }
 
     public override void _Process(double delta)
@@ -62,8 +85,22 @@ public partial class Game : Node3D
     public override void _Ready()
     {
         GD.Print("Spawner");
-        ZombieSpawner(true);
+        //ZombieSpawner(true);
         GD.Print("Spawner finished");
+        //this.mm = new MultiMesh();
+        //mm.TransformFormat = MultiMesh.TransformFormatEnum.Transform3D;
+        //mm.InstanceCount = 100;
+        //var texture = GD.Load<Texture2D>("res://Zombies/ZT1_idle.png");
+        //var material = new StandardMaterial3D();
+        //material.AlbedoTexture = texture;
+        //material.ShadingMode = BaseMaterial3D.ShadingModeEnum.Unshaded;
+        //material.BillboardMode = BaseMaterial3D.BillboardModeEnum.Enabled;
+        //var quad = new QuadMesh();
+        //quad.Material = material;
+        //mm.Mesh = quad;
+        MultiMeshInstance.Visible = true;
+        mm = MultiMeshInstance.Multimesh;
+        ZombieSpawnerMultiMesh(MultiMeshInstance.Multimesh, true);
     }
 
 
