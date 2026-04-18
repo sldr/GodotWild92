c2f3e8944e2a9c8a60a0587fc7c59e5b90badc9d
Gregory <ryanlemonds00@gmail.com>
2026-04-18 04:47:40 -0500

    Implemented fundamentals of shotgun capability

--- Bullet.cs
+++ Bullet.cs
@@ -7,6 +7,8 @@ public partial class Bullet : RigidBody3D
 	[Export] public float maxDistance;
 	[Export] public CharacterBody3D PlayerRef;
 
+	public Vector3 direction;
+
 	private Vector3 velocity;
 	private Vector3 startPosition;
 	private RayCast3D bulletRay;
@@ -16,10 +18,8 @@ public partial class Bullet : RigidBody3D
 	{
 		//bulletRay = GetNode<RayCast3D>("Area3D/RayCast3D");
 		//bulletRay.AddException(PlayerRef);
-		//startPosition = GlobalPosition;
+		startPosition = Position;
 		//bulletRay.TargetPosition = new Vector3(0, 0, -maxDistance);
-
-
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,23 +27,30 @@ public partial class Bullet : RigidBody3D
 	{
 		//GD.Print(bulletRay.TargetPosition);
 		//if (bulletRay.IsColliding())
-		
-		
 	}
 
     public override void _PhysicsProcess(double delta)
     {
-		Vector3 A = Vector3.Zero;
-		A.Z = -1 * speed;
-		this.MoveAndCollide(A);
+		if (Position.DistanceTo(this.startPosition) >= maxDistance)
+			this.QueueFree();
+        Game game = GetTree().CurrentScene as Game;
+		//GD.Print("Before direction: ", direction);
+		//direction = new Vector3 (direction.X, 0, direction.Z);
+        Vector3 A = direction * speed;
+        //A.Y = 1;
+        //GD.Print("After direction: ", direction);
+        this.MoveAndCollide(A);
 		var cols = this.GetCollidingBodies();
-		foreach (Node3D N in cols)
-		{
-			if (N is Zombie Z)
+		if(cols != null)
+			foreach (Node3D N in cols)
 			{
-				Z.
+				if (N is Zombie Z)
+				{
+					game.ZombieHit(Z);
+					GD.Print("Hit!");
+					this.QueueFree();
+				}
 			}
-		}
     }
 
 }
